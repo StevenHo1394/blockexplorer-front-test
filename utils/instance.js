da4bd05bb66dfdb6bc6833e9e11f739e8048c435
StevenHo1394 <StevenHo1394>
2021-11-28 14:56:55 +0000

    modified file

--- utils/instance.js
+++ utils/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL:'http://localhost:3000/api'
-    baseURL:'http://localhost:5001'	
+    baseURL:'http://165.232.173.187:5001'	
 })
 
 instance.interceptors.request.use(req => {
