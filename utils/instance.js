21a3294cbd89a5089b61f8a8c72775db8c6f8228
StevenHo1394 <75365123+StevenHo1394@users.noreply.github.com>
2022-01-13 14:00:55 +0800

    Update instance.js

--- utils/instance.js
+++ utils/instance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL:'http://localhost:3000/api'
-    baseURL:'http://165.232.173.187:5001'	
+    //baseURL:'http://165.232.173.187:5001'	
+    baseURL:'http://3.68.159.170:5001'
 })
 
 instance.interceptors.request.use(req => {
